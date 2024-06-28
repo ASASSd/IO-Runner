cc050531cd6dcaeba01aa09fa92af80e9aa0743b
Ilya Smirkin <ilyasmirkin200137@gmail.com>
2024-06-28 14:22:21 +0000

    First commit here

---
+++ rebuild_firmware.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+BUILD_DIR=$PWD/build
+ELF_FILENAME="neimark_1.elf"
+BIN_FILENAME="neimark_1.bin"
+
+if [ -d $BUILD_DIR ]; then
+  rm -rf $BUILD_DIR
+fi
+mkdir $BUILD_DIR
+cd $BUILD_DIR
+cmake ..
+make -j$(nproc)
+arm-none-eabi-objcopy -S -O binary $ELF_FILENAME $BIN_FILENAME
